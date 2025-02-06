a24e616d6e89653d7a3b359e3da72541c304450f
Theodor Coelho <theodorfcoelho@gmail.com>
2025-02-06 06:19:31 -0300

    feat: adiciona factory e models para geração de dados fake

--- ChallengeQA/Models/DataModels.cs
+++ ChallengeQA/Models/DataModels.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.DevTools.V130.Storage;
 
 namespace ChallengeQA.Models
 {
-    public class DataModels
+    public class Candidato
     {
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string NomeSocial { get; set; }
+        public string DataNascimento { get; set; }
+        public string TipoDeficiencia { get; set; }
+        public string Email { get; set; }
+        public string Celular { get; set; }
+        public string Telefone { get; set; }
+
+    }
+
+    public class Endereco
+    {
+        public string Cep { get; set; }
+        public string Rua { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Pais { get; set; }
     }
 }
--- ChallengeQA/Support/TestDataFactory.cs
+++ ChallengeQA/Support/TestDataFactory.cs
@@ -4,11 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Bogus;
+using Bogus.Extensions.Brazil;
 using ChallengeQA.Models;
+using FluentAssertions.Primitives;
+using static OpenQA.Selenium.BiDi.Modules.Session.ProxyConfiguration;
 
 namespace ChallengeQA.Support
 {
     public static class TestDataFactory
     {
+        private static readonly Faker Faker = new Faker("pt_BR");
+
+        public static Candidato CriarCandidato()
+        {
+            return new Candidato
+            {
+                //Cpf = Faker.Person.Cpf(includeFormatSymbols: false),
+                Cpf = GerarCpfInvalido(),
+                Nome = Faker.Name.FirstName(),
+                Sobrenome = Faker.Name.LastName(),
+                NomeSocial = Faker.Name.FirstName(),
+                DataNascimento = Faker.Date.Between(
+                    DateTime.Now.AddYears(-80),
+                DateTime.Now.AddYears(-18) )
+                    .ToString("dd/MM/yyyy"),
+                TipoDeficiencia = Faker.PickRandom("Física", "Auditiva", "Visual", "Intelectual", "Mental", "Sensorial", "Múltipla"),
+                Email = Faker.Internet.Email(),
+                Celular = Faker.Phone.PhoneNumber("(##) #####-####"),
+                Telefone = Faker.Phone.PhoneNumber("(##) #####-####")
+            };
+        }
+
+        public static Endereco CriarEndereco()
+        {
+            return new Endereco
+            {
+                Cep = Faker.Address.ZipCode("#####-###"),
+                Rua = Faker.Address.StreetName(),
+                Complemento = Faker.Address.SecondaryAddress(),
+                Bairro = Faker.Address.County(),
+                Cidade = Faker.Address.City(),
+                Estado = Faker.Address.StateAbbr(),
+                Pais = Faker.Address.Country()
+            };
+
+        }
+        private static string GerarCpfInvalido()
+        {
+            string cpfValido = Faker.Person.Cpf(includeFormatSymbols: false);
+            return cpfValido.Substring(0, cpfValido.Length - 2) + "00";
+        }
+
     }
 }
